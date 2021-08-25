9fe3d92fac9b9ec4c3abc8fd98febcdea9ec8a52
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-08-25 15:40:49 -0300

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import CreateTask from '../modals/CreateTask';
+import Card from '../components/Card'
+
+const TodoList = () => {
+    const [modal, setModal] = useState(false);
+    const [taskList, setTaskList] = useState([]);
+    
+
+    useEffect(() => {
+        let arr = localStorage.getItem('taskList');
+        
+        if(arr) {
+            let obj = JSON.parse(arr);
+            setTaskList(obj)
+        }
+    }, []);
+
+    const deleteTask = (index) => {
+        let tempList = taskList;
+        tempList.splice(index, 1);
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const updateListArray = (obj, index) => {
+        let tempList = taskList;
+        tempList[index] = obj;
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList);
+        window.location.reload();
+
+    }
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const saveTask = (taskObj) => {
+        let tempList = taskList;
+        tempList.push(taskObj);
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList);
+        setModal(false)
+    }
+
+    return (
+        <>
+        <div className="header text-center">
+            <h3>Task Manager</h3>
+            <button
+            onClick={() => setModal(true)} 
+            className="btn btn-primary mt-2">
+                Create Task
+            </button>
+        </div>
+
+        <div className="task-container">
+            {taskList && taskList.map((obj, index) => <Card taskObj={obj} index={index} deleteTask={deleteTask} updateListArray={updateListArray}/> )}
+        </div>
+        <CreateTask toggle={toggle} modal={modal} save={saveTask}/>
+        </>
+    );
+};
+
+export default TodoList;
